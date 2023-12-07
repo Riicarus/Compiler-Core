7708b6b5b153f05589df75f4b4a56141f3302fc5
Riicarus <riicarus.acc@gmail.com>
2023-12-07 16:45:54 +0800

    update: rearrange LL1Syntaxer's logic

--- src/main/java/io/github/riicarus/front/syntax/ll1/LL1Syntaxer.java
+++ src/main/java/io/github/riicarus/front/syntax/ll1/LL1Syntaxer.java
@@ -7,10 +7,7 @@ import io.github.riicarus.front.syntax.SyntaxProduction;
 import io.github.riicarus.front.syntax.SyntaxSymbol;
 import io.github.riicarus.front.syntax.Syntaxer;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.Stack;
+import java.util.*;
 
 /**
  * LL1 文法分析器.
@@ -22,90 +19,124 @@ import java.util.Stack;
 public class LL1Syntaxer implements Syntaxer {
 
     private final SyntaxDefiner definition;
+    private final Map<SyntaxSymbol, Map<String, Set<SyntaxProduction>>> analyzeMap;
 
     // 分析栈
     private final Stack<SyntaxSymbol> stack = new Stack<>();
+    private SyntaxSymbol topSymbol;
 
     private int tokenIdx;
-    private List<Token> tokens;
+    private Token curToken;
+    private final List<Token> tokens = new ArrayList<>();
+
+    private final Set<LexicalSymbol> assistantLexSymbolSet = new HashSet<>();
 
     public LL1Syntaxer(SyntaxDefiner definition) {
         this.definition = definition;
+        this.analyzeMap = definition.getAnalyzeMap();
 
         definition.load();
     }
 
     @Override
-    public void parse(List<Token> tokenList, Set<LexicalSymbol> assistantLexSymbolSet) {
-        tokens = tokenList;
+    public void parse(List<Token> tokenList, Set<LexicalSymbol> assistSet) {
+        if (!reset(tokenList, assistSet))
+            throw new IllegalStateException("LL1Syntax wrong: token list is null.");
 
-        final Map<SyntaxSymbol, Map<String, Set<SyntaxProduction>>> analyzeMap = definition.getAnalyzeMap();
-
-        stack.clear();
-        stack.push(definition.getEndSymbol());
-        stack.push(definition.getStartSymbol());
-
-        tokenIdx = 0;
-        while (!stack.isEmpty()) {
+        while (!stack.isEmpty() && !checkEnds()) {  // 如果分析栈不空, 并且没有遇到结束符
             System.out.println(stack);
+            System.out.println(curToken);
 
-            SyntaxSymbol top = stack.peek();
-
-            Token token;
-
-            // 处理结束符号, 栈顶为结束符号
-            if (top.equals(definition.getEndSymbol())) {
-                if (tokenIdx >= tokens.size())  // 如果 tokens 已经遍历完, 报错
-                    throw new IllegalStateException("LL1Syntax wrong: syntax not complete, want: \"" + top + "\" but get null.");
-                else {
-                    token = tokens.get(tokenIdx);
-                    if (token.getSymbol().getName().equals(definition.getEndSymbol().getName())) {  // 如果 tokens 也是结束符号, 就直接消去
-                        stack.pop();
-                        continue;
-                    } else    // 否则报错
-                        throw new IllegalStateException("LL1Syntax wrong: syntax complete, but get \"" + top + "\".");
-                }
-            }
-
-            token = tokens.get(tokenIdx);
-
-            System.out.println(token);
-
-            if (top.isTerminal()) {
-                if (top.equals(definition.getEpsilonSymbol())) {    // 空串直接弹出
-                    stack.pop();
-                } else if (top.getName().equals(token.getSymbol().getName())) { // 非空终结符弹出, 匹配下一个 token
-                    stack.pop();
-                    tokenIdx++;
-                    handleAssistantLexSymbols(assistantLexSymbolSet);
-                } else {    // 找不到对应的非终结符
-                    throw new IllegalStateException("LL1Syntax wrong: want : \"" + top.getName() + "\", get: \"" + token.getSymbol().getName() + "\"");
-                }
-            } else {
-                stack.pop();
-
-                final Map<String, Set<SyntaxProduction>> setMap = analyzeMap.get(top);
-                if (setMap == null || setMap.isEmpty())
-                    throw new IllegalStateException("LL1Syntax wrong: analyze map can not find symbol: \"" + top);
-
-                final Set<SyntaxProduction> productionSet = setMap.get(token.getSymbol().getName());
-                if (productionSet == null || productionSet.isEmpty())
-                    throw new IllegalStateException("LL1Syntax wrong: illegal token: " + token + " for current symbol: " + top);
-
-                productionSet.forEach(
-                        // 注意, LL1 文法中的 Set 实际上只会有一个产生式, 这里只循环一次.
-                        p -> {
-                            for (int j = p.getBody().size() - 1; j >= 0; j--) {
-                                stack.push(p.getBody().get(j));
-                            }
-                        });
-            }
+            if (topSymbol.isTerminal())
+                handleTerminal();
+            else
+                handleNonterminal();
         }
 
         System.out.println(stack);
     }
 
-    private void handleAssistantLexSymbols(Set<LexicalSymbol> assistantLexSymbolSet) {
-        while (tokenIdx < tokens.size() && assistantLexSymbolSet.contains(tokens.get(tokenIdx).getSymbol())) tokenIdx++;
+    @SuppressWarnings("all") // 抑制空 while 循环
+    private void nextTokenWithAssitant() {
+        while (nextToken() && assistantLexSymbolSet.contains(tokens.get(tokenIdx).getSymbol())) ;
+    }
+
+    private boolean nextToken() {
+        if (tokenIdx < tokens.size()) {
+            tokenIdx++;
+            curToken = tokens.get(tokenIdx);
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean reset(List<Token> tokenList, Set<LexicalSymbol> assistSet) {
+        tokenIdx = -1;
+        curToken = null;
+        topSymbol = null;
+
+        tokens.clear();
+        tokens.addAll(tokenList);
+
+        if (!nextToken()) return false;
+
+        assistantLexSymbolSet.clear();
+        assistantLexSymbolSet.addAll(assistSet);
+
+        stack.clear();
+        stack.push(definition.getEndSymbol());
+        stack.push(definition.getStartSymbol());
+
+        return true;
+    }
+
+    private boolean checkEnds() {
+        topSymbol = stack.peek();
+
+        if (topSymbol.equals(definition.getEndSymbol())) {
+            if (tokenIdx >= tokens.size())  // 如果 tokens 已经遍历完, 报错
+                throw new IllegalStateException("LL1Syntax wrong: syntax not complete, want: \"" + topSymbol + "\" but get null.");
+            else {
+                if (curToken.getSymbol().getName().equals(definition.getEndSymbol().getName())) {  // 如果 tokens 也是结束符号, 就直接消去
+                    stack.pop();
+                    return true;
+                } else    // 否则报错
+                    throw new IllegalStateException("LL1Syntax wrong: syntax complete, but get \"" + topSymbol + "\".");
+            }
+        }
+
+        return false;
+    }
+
+    private void handleTerminal() {
+        if (topSymbol.equals(definition.getEpsilonSymbol())) {    // 空串直接弹出
+            stack.pop();
+        } else if (topSymbol.getName().equals(curToken.getSymbol().getName())) { // 非空终结符弹出, 匹配下一个 token
+            stack.pop();
+            nextTokenWithAssitant();
+        } else {    // 找不到对应的非终结符
+            throw new IllegalStateException("LL1Syntax wrong: want : \"" + topSymbol.getName() + "\", get: \"" + curToken.getSymbol().getName() + "\"");
+        }
+    }
+
+    private void handleNonterminal() {
+        stack.pop();
+
+        Map<String, Set<SyntaxProduction>> setMap = analyzeMap.get(topSymbol);
+        if (setMap == null || setMap.isEmpty())
+            throw new IllegalStateException("LL1Syntax wrong: analyze map can not find symbol: \"" + topSymbol);
+
+        Set<SyntaxProduction> productionSet = setMap.get(curToken.getSymbol().getName());
+        if (productionSet == null || productionSet.isEmpty())
+            throw new IllegalStateException("LL1Syntax wrong: illegal token: " + curToken + " for current symbol: " + topSymbol);
+
+        productionSet.forEach(
+                // 注意, LL1 文法中的 Set 实际上只会有一个产生式, 这里只循环一次.
+                p -> {
+                    for (int j = p.getBody().size() - 1; j >= 0; j--) {
+                        stack.push(p.getBody().get(j));
+                    }
+                });
     }
 }
